251d13e796a2d26c99c58400eb1f1bf270f63378
chiuzon <89428507+chiuzon@users.noreply.github.com>
2021-09-27 20:15:24 +0300

    Images fixed

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,7 +6,7 @@ const NotFoundPage = () => {
   return (
     <div className='w-full flex justify-center items-center relative h-screen'>
       <SEO title="404 Not Found" description="404 Not Found page" />
-      <img src={'/imgs/header-image.jpg'} className='absolute opacity-25 w-full pointer-events-none object-cover h-full' />
+      <img src={'header-image.jpg'} className='absolute opacity-25 w-full pointer-events-none object-cover h-full' />
 
       <div className='flex flex-col justify-center items-center text-4xl space-y-5 flex-shrink'>
         <h1 className='font-skranji font-bold text-center'>404 Not Found</h1>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import * as React from "react"
 import SEO from "../components/SEO"
 
+import logo from "../images/header-image.jpg"
+
 // markup
 const IndexPage = () => {
   return (
    <div className='w-full flex justify-center items-center relative h-screen'>
       <SEO title="Home" description="Rarity.plus is an upcoming 3D MMORPGer built on top of Rarity Smart Contracts" />
       
-      <img src={'/imgs/header-image.jpg'} className='absolute opacity-25 w-full pointer-events-none object-cover h-full' />
+      <img src={'header-image.jpg'} className='absolute opacity-25 w-full pointer-events-none object-cover h-full' />
 
       <div className='flex flex-col justify-center items-center text-4xl space-y-5 flex-shrink'>
           <h1 className='font-skranji font-bold text-center'>rarity.plus</h1>
