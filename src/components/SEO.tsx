db7e987c354350b4c06d3ecf6879d2d041294682
chiuzon <89428507+chiuzon@users.noreply.github.com>
2021-09-27 20:43:15 +0300

    title fix

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -11,7 +11,7 @@ const SEO = ({title, description}: SEOProps) => {
         <Helmet>
             <title>{title} | rarity.plus</title>
             <meta name="description" content={description || "Rarity.plus is an upcoming 3D MMORPGer built on top of Rarity Smart Contracts"} />
-            <meta property="og:title" content={title + "rarity.plus"} />
+            <meta property="og:title" content={title + "| rarity.plus"} />
             <meta property="og:description" content={description || "Rarity.plus is an upcoming 3D MMORPGer built on top of Rarity Smart Contracts"} />
             <meta property="og:image" content="/icon.png" />
         </Helmet>
