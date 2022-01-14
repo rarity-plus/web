b41db922a10ee230badcb373b25b90ab7ffbcac7
as <89428507+chiuzon@users.noreply.github.com>
2022-01-14 14:04:27 +0200

    Remove gatsby js

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: [
-        `gatsby-plugin-postcss`,
-        `gatsby-plugin-typescript`,
-        `gatsby-plugin-react-helmet`
-    ]
-}
--- src/configs/links.ts
+++
@@ -1,6 +0,0 @@
-
-
-export const SOCIAL_LINKS = {
-    DISCORD: "https://discord.gg/DSdBKmfdBf",
-    TWITTER: "https://twitter.com/rarity_plus"
-}
